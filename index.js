3a384251a9d460e3805f30b97b7c740be67536ed
Baptiste Szopny <b.szopny@gmail.com>
2019-10-06 17:44:32 +0200

    Add Guild Member Add + Custom command

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on("guildMemberAdd", (member) => {
+    let user = client.users.find(x=> x.id === member.id).username
+    let tag = client.users.find(x=> x.id === member.id).discriminator
+    let imageurl= client.users.find(x=> x.id === member.id).avatarURL
+    const exampleEmbed = new Discord.RichEmbed()
+    .setColor('#00FF00')
+    .setAuthor('Nouvelle arrivée sur le discord !')
+    .setThumbnail(imageurl)
+    .addField(user +"#"+ tag, "est arrivé sur le discord")
+    .setTimestamp()
+    .setFooter('© Clubat');
+    client.channels.find("id","551762440182824983").send(exampleEmbed)
+  });
+
+client.on('message', message => {
+    if (message.content === '.salut') {
+      message.channel.send('Salut les négros !')
+    }
+  })
+
 client.login(process.env.TOKEN);
